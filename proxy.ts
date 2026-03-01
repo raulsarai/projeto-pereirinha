010db753607f9bea227b92375ee09d7c36f1e5af
Raul Mauro <81999143+raulsarai@users.noreply.github.com>
2026-03-01 11:27:18 -0300

    Delete proxy.ts

--- proxy.ts
+++
@@ -1,7 +0,0 @@
-// proxy.ts na raiz do projeto
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
-
-export function proxy(request: NextRequest) {
-  return NextResponse.next()
-}
