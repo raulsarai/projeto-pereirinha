752100b46c536564cb9ce048bb945257701ba93b
Raul Mauro <81999143+raulsarai@users.noreply.github.com>
2026-03-01 11:24:18 -0300

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,14 +1,7 @@
-// proxy.ts
+// proxy.ts na raiz do projeto
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
 export function proxy(request: NextRequest) {
-  // se não quiser fazer nada, só deixa passar
   return NextResponse.next()
 }
-
-// opcional: limite os paths onde roda
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
-}
-
